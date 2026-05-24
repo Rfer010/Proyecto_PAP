4af7c7ad570a195e97e73463879da197a5e2a744
Rfer010 <gr26003@ues.edu.sv>
2026-05-24 08:41:30 -0600

    inicio de proyecto en NetBeans v29

---
+++ jerarquia_empleados/src/main/java/jerarquia_empleados/Jerarquia_empleados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jerarquia_empleados;
+
+/**
+ *
+ * @author ferna
+ */
+public class Jerarquia_empleados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
